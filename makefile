26bb15b4dacb6849c3a570b16e1b147f751d0408
adrian <adrian.lin.1.0.0@gmail.com>
2023-02-09 13:52:38 +0800

    chore: using pytest instead of unittest

--- makefile
+++ makefile
@@ -1,4 +1,4 @@
 install:
 	pip3 install -r requirements.txt
 unit-test:
-	python3 tests/unit/*_test.py
+	pytest
--- tests/unit/utils_test.py
+++ tests/unit/utils_test.py
@@ -1,11 +1,5 @@
-import unittest
-from context import utils
+from .context import utils
 
-class UtilsTest(unittest.TestCase):
-    def test_say_hello(self):
-        self.assertEqual(utils.Say_hello("Adrian"), "Hello Adrian")
-        self.assertEqual(utils.Say_hello("Zoey"), "Hello Zoey")
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_say_hello():
+        assert utils.Say_hello("Adrian") == "Hello Adrian"
+        assert utils.Say_hello("Zoey") == "Hello Zoey"
